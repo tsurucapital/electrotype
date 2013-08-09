0455278e051a33993c69e6842f71964dd61e86e0
Andrew Richards <randrew@gmail.com>
2013-08-09 12:29:06 +0900

    Compile shader strings into binary.
    This change causes the strings for the shaders to be compiled into the
    binary during cabal build. When a user of this library desires, he or
    she may invoke withTempShaderPaths, which causes the necessary shader
    files to be written into a temporary directory, and then passes the
    paths to the user code.
    
    This way, we don't have to worry about the location and distribution of
    static data files (previous handled via cabal install, which meant any
    user of the binary needed to have installed it via cabal install), but
    we also have the possibility of reloading shader files during runtime.
    A debugger or artist may tweak the temporary shader files and invoke
    some routine to re-read them, instead of being forced to recompile the
    library and application code before seeing the changes.
    
    The code in the cabal Setup.hs is a bit hacky and could definitely be
    improved.

--- Graphics/Rendering/Electrotype.hs
+++ Graphics/Rendering/Electrotype.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module Graphics.Rendering.Electrotype
-( module Graphics.Rendering.Electrotype.Internal.Shader
-, module Graphics.Rendering.Electrotype.Internal.VertexBuffer
+( module Graphics.Rendering.Electrotype.Internal.VertexBuffer
 , module Graphics.Rendering.Electrotype.Internal.TextureFont
+, module Graphics.Rendering.Electrotype.Baked.Shaders
 ) where
 
-import Graphics.Rendering.Electrotype.Internal.Shader
 import Graphics.Rendering.Electrotype.Internal.TextureFont
 import Graphics.Rendering.Electrotype.Internal.VertexBuffer
+import Graphics.Rendering.Electrotype.Baked.Shaders
--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,32 @@
 import Distribution.Simple
-main = defaultMain
+import System.Directory
+import System.IO
+import System.FilePath
+
+internalPath, bakedPath, shadersDirPath :: FilePath
+internalPath = "Graphics" </> "Rendering" </> "Electrotype" </> "Internal"
+bakedPath = "Graphics" </> "Rendering" </> "Electrotype" </> "Baked"
+shadersDirPath = "src" </> "shaders"
+
+readVertAndFrag :: String -> IO (String, String)
+readVertAndFrag shaderName = do
+    vert <- readFile $ shadersDirPath </> shaderName ++ ".vert"
+    frag <- readFile $ shadersDirPath </> shaderName ++ ".frag"
+    return (vert, frag)
+
+showVertAndFrag :: String -> (String, String) -> String
+showVertAndFrag staticName (vert, frag) = "\n"
+    ++ staticName ++ "Vert = " ++ show vert ++ "\n"
+    ++ staticName ++ "Frag = " ++ show frag ++ "\n"
+
+main = do
+    defaultMainWithHooks $ simpleUserHooks
+        { preBuild = \args buildflags -> do
+            putStrLn "Baking shader strings..."
+            template <- readFile $ bakedPath </> "Shaders.hs_template"
+            shader1 <- readVertAndFrag "v3f-t2f-c4f"
+            writeFile (bakedPath </> "Shaders.hs") $
+                template ++ showVertAndFrag "v3ft2fc4f" shader1
+
+            (preBuild simpleUserHooks) args buildflags
+        }
